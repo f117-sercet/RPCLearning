4408323aa4e1796d37bd2d65b92591e9154fe6ab
sowrd <15159885771@163.com>
2023-03-04 18:39:32 +0800

    Rpc框架

---
+++ hello-service-api/src/main/java/com/rpcTest/Hello.java
@@ -0,0 +1,23 @@
+package com.rpcTest;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * Description： TODO
+ *
+ * @author: 段世超
+ * @aate: Created in 2023/3/4 18:37
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class Hello implements Serializable {
+
+    private String message;
+    private String description;
+}
---
+++ hello-service-api/src/main/java/com/rpcTest/HelloService.java
@@ -0,0 +1,12 @@
+package com.rpcTest;
+
+/**
+ * Description： TODO
+ *
+ * @author: 段世超
+ * @aate: Created in 2023/3/4 18:38
+ */
+public interface HelloService {
+
+    String hello(Hello hello);
+}
